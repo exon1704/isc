09e6f6082792a8defec6e2dbdf80a23899636f7e
Pablo Reyes <p.reyes1704@gmail.com>
2023-12-21 08:20:44 -0600

    Se eliminan importaciones redundantes
    Las implementaciones ya se definen en la declaración de sharedmodule y appmodule

--- src/app/layout/app.layout.module.ts
+++ src/app/layout/app.layout.module.ts
@@ -1,31 +1,16 @@
 import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {FormsModule} from '@angular/forms';
 import {RouterModule} from "@angular/router";
 import {AppMenuComponent} from "@isc/layout/app.menu.component";
 import {AppMenuitemComponent} from "@isc/layout/app.menuitem.component";
 import {AppTopBarComponent} from "@isc/layout/app.topbar.component";
 import {AppSidebarComponent} from "@isc/layout/app.sidebar.component";
 import {AppLayoutComponent} from "@isc/layout/app.layout.component";
-import {HttpClientModule} from "@angular/common/http";
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {CommonModule} from "@angular/common";
 
 @NgModule({
-  declarations: [
-    AppMenuitemComponent,
-    AppTopBarComponent,
-    AppMenuComponent,
-    AppSidebarComponent,
-    AppLayoutComponent,
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpClientModule,
-    BrowserAnimationsModule,
-    RouterModule,
-  ],
-  exports: [AppLayoutComponent]
+  declarations: [AppMenuitemComponent, AppTopBarComponent, AppMenuComponent, AppSidebarComponent, AppLayoutComponent],
+  imports: [RouterModule, CommonModule],
+  exports: [AppLayoutComponent, AppTopBarComponent]
 })
 export class AppLayoutModule {
 }
