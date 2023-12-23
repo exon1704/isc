c4f9bccc994e58d3c17bfc015d93f867cc4c594b
Pablo Reyes <p.reyes1704@gmail.com>
2023-12-23 14:00:26 -0600

    Soporte para error-component, y definición de progresspinner

--- src/app/modulo/folio/administrar/administrar.module.ts
+++ src/app/modulo/folio/administrar/administrar.module.ts
@@ -2,19 +2,18 @@ import {NgModule} from '@angular/core';
 
 import {AdministrarRoutingModule} from './administrar-routing.module';
 import {AdministrarComponent} from './administrar.component';
-import {ErrorComponent} from "@isc/shared/components/error/error.component";
+import {ErrorComponent} from "@isc/core/shared/components/error/error.component";
 import {ToolbarModule} from "primeng/toolbar";
 import {DropdownModule} from "primeng/dropdown";
 import {CalendarModule} from "primeng/calendar";
 import {TableModule} from "primeng/table";
-import {ProgressSpinnerModule} from "primeng/progressspinner";
 import {InputGroupModule} from "primeng/inputgroup";
 import {SharedModule} from "@isc/shared/shared.module";
 import {PaginatorModule} from "primeng/paginator";
 
 @NgModule({
   declarations: [AdministrarComponent],
-  imports: [AdministrarRoutingModule, ErrorComponent, ToolbarModule, DropdownModule, CalendarModule, TableModule, ProgressSpinnerModule, InputGroupModule, SharedModule, PaginatorModule,]
+  imports: [AdministrarRoutingModule, ErrorComponent, ToolbarModule, DropdownModule, CalendarModule, TableModule, InputGroupModule, SharedModule, PaginatorModule,]
 })
 export class AdministrarModule {
 }
