8ea4ef8adde4f14ec8301685d475d6438388ff08
Pablo Reyes <p.reyes1704@gmail.com>
2024-01-10 00:57:20 -0600

    Se actualiza declaración de interfaz

--- src/app/core/commons/paginator.ts
+++ src/app/core/commons/paginator.ts
@@ -1,7 +1,7 @@
 export interface Paginator {
-   first: number | 0;
-   rows: number | 50,
-   page: number | 0,
-   totalElements: number | 0,
-   pageCount: number | 0
+   first: number;
+   rows: number;
+   page: number;
+   totalElements: number;
+   pageCount: number;
 }
